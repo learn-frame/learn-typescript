626031f7006fe7055ac07028f319225b8e354149
yanceyleo <yanceyofficial@gmail.com>
2019-10-30 08:10:34 +0800

    feat: finish enum part

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # learn-typescript
 
 Learn TypeScript with GeekTime course.
+
+## 静态语言和动态语言
+
+静态类型语言：在**编译**阶段确定所有变量的类型
+动态类型语言：在**执行**阶段确定所有变量的类型
--- src/enum.ts
+++ src/enum.ts
@@ -14,11 +14,11 @@ enum Direction1 {
   Right,
 }
 
-// 0 1 3 4
+// 0 1 4 5
 enum Direction2 {
   Up,
   Down,
-  Left = 3,
+  Left = 4,
   Right,
 }
 
@@ -30,7 +30,7 @@ enum Direction3 {
   Right,
 }
 
-// value 既可以是 number 当有一个为字符串时
+// value 既可以是 number，也可以是字符串
 enum Message {
   Success = 'やったー',
   Fail = 'しっぱい',
@@ -46,5 +46,47 @@ enum Week {
   Sunday = 7,
 }
 
+// 反向引用
 const monday = Week[Week.Monday]
 console.log(monday)
+
+// 枚举成员的性质
+enum Type {
+  // 常量枚举成员，在编译阶段就被计算了
+  a,
+  b = Week.Thursday,
+  c = 1 + 1,
+  // 可计算的枚举成员，需要等到执行阶段
+  d = Math.random(),
+  e = 'abc'.length,
+}
+
+/*
+ * "use strict";
+ * var Type;
+ * (function (Type) {
+ *     // 常量枚举成员，在编译阶段就被计算了
+ *     Type[Type["a"] = 0] = "a";
+ *     Type[Type["b"] = Week.Thursday] = "b";
+ *     Type[Type["c"] = 2] = "c";
+ *     // 可计算的枚举成员，需要等到执行阶段
+ *     Type[Type["d"] = Math.random()] = "d";
+ *     Type[Type["e"] = 'abc'.length] = "e";
+ * })(Type || (Type = {}));
+ */
+
+// 常量枚举会在编译阶段被移除
+// 当不需要对象，而只需要对象的值的时候可以使用常量枚举
+const enum ConstEnum {
+  UP,
+  DOWN,
+}
+
+// 枚举也可以作为一种类型
+// 基本也没这么用的。。。
+enum TypedEnum {
+  UP,
+  DOWN,
+}
+
+const up: TypedEnum = 1
