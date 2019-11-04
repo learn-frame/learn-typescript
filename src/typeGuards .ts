6582e01eb3d0c12c21b38e7d9ed01df15f4428a5
yanceyleo <yanceyofficial@gmail.com>
2019-11-04 08:25:06 +0800

    feat: finish typeGuards

--- src/typeGuards .ts
+++ src/typeGuards .ts
@@ -1,9 +1,13 @@
+// TypeScript 能保证变量在特定区块中是确定的类型，此之谓类型保护
+
 enum Type {
   Strong,
   Week,
 }
 
 class Java {
+  private java = 'java'
+
   public helloJava() {
     console.log('hello Java')
   }
@@ -15,7 +19,12 @@ class JavaScript {
   }
 }
 
-function getLanguage(type: Type) {
+// lang is Java 称作类型谓词
+function isJava(lang: Java | JavaScript): lang is Java {
+  return !!(lang as Java).helloJava
+}
+
+function getLanguage(type: Type, x: string | number) {
   let langInstance = type === Type.Strong ? new Java() : new JavaScript()
 
   // 各种类型断言，不优雅
@@ -23,6 +32,34 @@ function getLanguage(type: Type) {
     ;(langInstance as Java).helloJava()
   } else {
     ;(langInstance as JavaScript).helloJavaScript()
+  }
+
+  // 使用 instanceof
+  if (langInstance instanceof Java) {
+    langInstance.helloJava()
+  } else {
+    langInstance.helloJavaScript()
+  }
+
+  // 使用 in
+  if ('java' in langInstance) {
+    langInstance.helloJava()
+  } else {
+    langInstance.helloJavaScript()
+  }
+
+  // 使用 typeof
+  if (typeof x === 'string') {
+    console.log(x.length)
+  } else {
+    console.log(x.toFixed())
+  }
+
+  // 通过创建类型保护函数
+  if (isJava(langInstance)) {
+    langInstance.helloJava()
+  } else {
+    langInstance.helloJavaScript()
   }
 
   return langInstance
