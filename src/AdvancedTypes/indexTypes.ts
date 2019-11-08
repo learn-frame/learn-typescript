475594223d9e9ce642bf635a17198623894b85f9
yanceyleo <yanceyofficial@gmail.com>
2019-11-08 09:17:36 +0800

    styles: formart code strctures

---
+++ src/AdvancedTypes/indexTypes.ts
@@ -0,0 +1,26 @@
+// 引子
+const o = {
+  a: 1,
+  b: 2,
+  c: 3,
+}
+
+const getValueList = (o: any, keys: string[]) => {
+  return keys.map(key => o[key])
+}
+
+getValueList(o, ['a', 'b']) // [1, 2]
+
+// 这样显然做不到对对象的约束
+getValueList(o, ['a', 'e']) // [1, undefined]
+
+// 索引类型的查询操作符 keyof T
+
+interface Obj {
+  a: string
+  b: boolean
+}
+
+const key: keyof Obj = ''
+
+// 索引类型的访问操作符 T[K]
