d4a61db4c304da31263fefcc64e65e9abf8b38c4
yanceyleo <yanceyofficial@gmail.com>
2019-11-17 17:01:46 +0800

    feat: 🎸 update tsconfig.json

---
+++ src/Modules/index.commonjs.ts
@@ -0,0 +1,9 @@
+const es6Obj = require('./index.es6')
+
+function someFn() {
+  console.log('haha')
+}
+
+const commonjsStr = 'yancey'
+
+module.exports = commonjsStr
---
+++ src/Modules/index.es6.ts
@@ -0,0 +1,5 @@
+// import someFn from './index.commonjs'
+
+export const es6Obj = {
+  name: 'yancey',
+}
