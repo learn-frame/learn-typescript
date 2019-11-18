c68f748105d89e23c2bc699359f05cdd0042502e
YanceyOfficial <yanceyofficial@gmail.com>
2019-11-18 16:08:56 +0800

    docs: update README.md for Enum

--- src/Enums/enum.ts
+++ src/Enums/enum.ts
@@ -30,7 +30,7 @@ enum Direction3 {
   Right,
 }
 
-// value 既可以是 number，也可以是字符串
+// value 既可以是 number, 也可以是字符串
 enum Message {
   Success = 'やったー',
   Fail = 'しっぱい',
@@ -52,11 +52,11 @@ console.log(monday)
 
 // 枚举成员的性质
 enum Typess {
-  // 常量枚举成员，在编译阶段就被计算了
+  // 常量枚举成员, 在编译阶段就被计算了
   a,
   b = Week.Thursday,
   c = 1 + 1,
-  // 可计算的枚举成员，需要等到执行阶段
+  // 可计算的枚举成员, 需要等到执行阶段
   d = Math.random(),
   e = 'abc'.length,
 }
@@ -65,25 +65,24 @@ enum Typess {
  * "use strict";
  * var Types;
  * (function (Types) {
- *     // 常量枚举成员，在编译阶段就被计算了
+ *     // 常量枚举成员, 在编译阶段就被计算了
  *     Types[Types["a"] = 0] = "a";
  *     Types[Types["b"] = Week.Thursday] = "b";
  *     Types[Types["c"] = 2] = "c";
- *     // 可计算的枚举成员，需要等到执行阶段
+ *     // 可计算的枚举成员, 需要等到执行阶段
  *     Types[Types["d"] = Math.random()] = "d";
  *     Types[Types["e"] = 'abc'.length] = "e";
  * })(Types || (Types = {}));
  */
 
 // 常量枚举会在编译阶段被移除
-// 当不需要对象，而只需要对象的值的时候可以使用常量枚举
+// 当不需要对象, 而只需要对象的值的时候可以使用常量枚举
 const enum ConstEnum {
   UP,
   DOWN,
 }
 
 // 枚举也可以作为一种类型
-// 基本也没这么用的。。。
 enum TypedEnum {
   UP,
   DOWN,
