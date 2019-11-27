f25b4882375bbeee1182f195350005eb0b1038c3
yanceyleo <yanceyofficial@gmail.com>
2019-11-28 07:21:45 +0800

    config: use awesome-typescript-loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -17,17 +17,11 @@ module.exports = {
       {
         test: /\.tsx?$/i,
         use: [
-          // {
-          //   loader: 'ts-loader',
-          //   options: {
-          //     // 开启 transpileOnly 会提高编译速度
-          //     // 但不会再编译过程中进行类型检查
-          //     transpileOnly: true,
-          //   },
-          // },
           {
-            loader: 'awesome-typescript-loader',
+            loader: 'ts-loader',
+            // loader: 'awesome-typescript-loader',
             options: {
+              // 开启 transpileOnly 会提高编译速度，但不会再编译过程中进行类型检查
               transpileOnly: true,
             },
           },
@@ -41,12 +35,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
-    // 通过这个 plugin 可以在 transpileOnly: true 的情况下仍然进行类型检查
-    // 它会把类型检查放到独立的进程中
-    // new ForkTsCheckerWebpackPlugin(),
+    // ForkTsCheckerWebpackPlugin 可以在 ts-loader 使用 transpileOnly: true 的情况下仍然进行类型检查
+    new ForkTsCheckerWebpackPlugin(),
 
-    // awesome-typescript-loader 配套的 Plugin
-    new CheckerPlugin(),
+    // CheckerPlugin 则是 awesome-typescript-loader ts-loader 使用 transpileOnly: true 的情况下仍然进行类型检查
+    // new CheckerPlugin(),
   ],
   optimization: {
     splitChunks: {
