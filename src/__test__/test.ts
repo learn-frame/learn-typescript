3d147952a93241911313321cbd6aa8963c3abfa9
YanceyOfficial <yanceyofficial@gmail.com>
2019-11-27 11:39:42 +0800

    feat: learn ts-loader

--- src/__test__/test.ts
+++ src/__test__/test.ts
@@ -11,3 +11,26 @@ interface Comment {
 }
 
 type T = Exclude<keyof Article, keyof Comment>
+
+interface User {
+  id: number
+  username: string
+  password: string
+}
+
+const user: User = {
+  id: 1,
+  username: 'yancey',
+  password: '12345',
+}
+
+type LiteUser = Omit<User, 'password'>
+
+const checker = (username: string, password: string): LiteUser | null => {
+  if (username === user.username && password === user.password) {
+    const { password, ...rest } = user
+    return rest
+  } else {
+    return null
+  }
+}
